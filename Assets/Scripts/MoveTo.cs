bcc12e781ae9cc9575edc116e9f5f8e3778dd1ab
Axolian <thedarkling3@gmail.com>
2015-11-11 10:08:28 -0600

    Week 8 Stuff

---
+++ Assets/Scripts/MoveTo.cs
@@ -0,0 +1,22 @@
+﻿// MoveTo.cs
+using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour {
+
+	// Use this for initialization
+
+	public Transform goal;
+	NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.destination = goal.position;
+
+	}
+}
